fb24bc83f5062cc8498f7cdbfb179e920949b69b
Qu4k3r <neves0007@gmail.com>
2021-03-02 20:40:29 -0300

    Exercicios 1 ao 10

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+
+import About from './components/About';
 import Home from './components/Home';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <main>
+        <Router>
+
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/users">Users</Link></li>
+          </ul>
+        </nav>
+
+          <Switch>
+            <Route path="/about" component={ About } />
+            <Route exact path="/users" component={ Users } />
+            <Route
+              exact path="/users/:id"
+              render={ (props) => (
+                <Users
+                  { ...props }
+                  greetingMessage="Good Morning"
+                  id={ props.match.params.id }
+                /> )}
+            />
+            <Route exact path="/" component={ Home } />
+          </Switch>
+
+        </Router>
+      </main>
     );
   }
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+// arquivo Users.js
+import React from 'react';
 
-class Users extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Users</h2>
-        <p> My awesome Users component </p>
-      </div>
-    );
-  }
+const Users = ({ greetingMessage = 'Hi There', id }) => {
+  if (id) return <p> {greetingMessage}, User number { id } </p>;
+  return (<div>
+    <h2> Users </h2>
+      <p> {greetingMessage}, this is my awesome Users component</p>
+    </div>
+  )
 };
 
 export default Users;
+
+// import React, { Component } from 'react';
+
+// class Users extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h2>Users</h2>
+//         <p> My awesome Users component </p>
+//       </div>
+//     );
+//   }
+// };
+
+// export default Users;
